799a58a708b718baa4115b059538e278938900a0
EmilyNorena <emily.norena-c@mail.escuelaing.edu.co>
2025-02-03 12:06:10 -0500

    pushed test

---
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -0,0 +1,19 @@
+package edu.eci.cvds.tdd.library;
+
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.loan.Loan;
+import edu.eci.cvds.tdd.library.user.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Library responsible for manage the loans and the users.
+ */
+public class LibraryTest {
+
+   
+
+}
